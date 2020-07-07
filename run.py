b8c085914f3e3a9d156e63d11434849f49d2578c
BuRny <gamingburny@gmail.com>
2020-07-07 22:19:09 +0200

    Fix run.py

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ class BotRunner:
     async def start_bot(self):
         current_folder = Path(__file__).parent
         bot_file_path = current_folder / "bot.py"
-        command = f"/usr/local/bin/python3.8 -m poetry run python {bot_file_path.absolute()}"
+        command = f"python3 -m poetry run python {bot_file_path.absolute()}"
         self.bot_process = subprocess.Popen(command.split(" "))
         logger.info(f"Starting bot on pid {self.bot_process.pid}")
 
